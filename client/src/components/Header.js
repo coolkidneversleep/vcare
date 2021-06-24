787425ac17735956fe8825cbb147aeb562617a1f
coolkidneversleep <iiippbhy@gmail.com>
2021-06-24 14:43:09 +0700

    fixed frontend

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,28 +1,27 @@
 import * as React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { Component } from 'react/cjs/react.production.min';
+import "../style/header.scss";
 
 
-class Header extends Component {
-  render() {
-  return(
-      <div style = {{ flexGrow: 1, margin: 0}}>
-  <AppBar>
-    <Toolbar>
-      <Typography variant="h6" style={{ flexGrow: 1 }}>
-        V CARE
-      </Typography>
-      <Button style={{textTransform:"capitalize"}}color="secondary" size="small" variant="text">Home</Button>
-      <Button style={{textTransform:"capitalize"}}color="secondary" size="small">Reservation</Button>
-      <Button style={{textTransform:"capitalize"}}color="secondary" size="small">Sign Out</Button>
-    </Toolbar>
-  </AppBar>
-      </div >
-    );
-      }
-  }
+const Header = () => {
+  
+  return (
+    <div className="nav" style={{ flexGrow: 1, margin: 0, marginTop: 12 }}>
+
+        <Toolbar>
+          <Typography variant="h6" className="logo" style={{paddingLeft: 50}}>
+            V CARE
+          </Typography>
+          <button className="btn">Home</button>
+          <button className="btn">Reservation</button>
+          <button className="btn">Sign Out</button>
+
+        </Toolbar>
+    </div >
+  );
+}
+
 
 export default Header;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,31 +2,28 @@ import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { fontSize, fontWeight } from '@material-ui/system';
+import '../style/landing.scss'
 class Landing extends Component {
     render() {
         return (
             <div style={{ paddingTop: 14 }}>
                 <Grid
-                    style={{padding:40}}
+                    style={{padding:40, paddingTop:0}}
                     container
                     direction="row"
                     justify="center"
-                    alignItems="center"
+                    alignItems="start"
                 >
-                    <img style={{width:552, height:552}} src="https://image.freepik.com/free-vector/illustration-doctor-injecting-vaccine-patient-clinic_23-2148865135.jpg"></img>
-                    <Grid direction="column">
+                    <img style={{width:552, height:552, marginLeft:40}} src="https://image.freepik.com/free-vector/illustration-doctor-injecting-vaccine-patient-clinic_23-2148865135.jpg"></img>
+                    <Grid direction="column" style={{paddingTop: 110, paddingLeft: 80 }}>
                     <div>
-                    <Typography color="secondary" style={{alignItems:"center", padding: 30, paddingTop:0, paddingBottom:0, fontSize: 40, fontWeight: "bold" }}>Prevent Better</Typography>
-                    <Typography color="secondary" style={{alignItems:"center", paddingLeft:30, fontSize: 40, fontWeight: "bold" }}>Than Care</Typography>
+                    <h2 className="welcome_text">Prevent Better</h2>
+                    <h2 className="welcome_text">Than Care </h2>
                     </div>
-                    <Button variant="outlined" color="secondary" style={{borderRadius:16, width:300, marginLeft:30}}>
-                        <Grid direction="row" alignItems="center" >
-                            <img style={{ width:20, height:20, marginTop:5}} src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"></img>
-                            <Typography variant="caption"color="secondary.light" style={{paddingTop:0,paddingLeft:10,textTransform:"capitalize",fontWeight:"bold"}}>Sign in with google</Typography>
-                        </Grid>
-                    </Button>
+                    <button className="signin_btn">
+                            <img style={{ width:20, height:20}} src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"></img>
+                            <Typography variant="caption" className="signin_text">Sign in with google</Typography>
+                    </button>
                     </Grid>
                 </Grid>
             </div>
