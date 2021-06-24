19d694c2557b8b3361d88e157dfc3f7f6f583522
coolkidneversleep <iiippbhy@gmail.com>
2021-06-24 15:03:13 +0700

    fixed landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -14,15 +14,15 @@ class Landing extends Component {
                     justify="center"
                     alignItems="start"
                 >
-                    <img style={{width:552, height:552, marginLeft:40}} src="https://image.freepik.com/free-vector/illustration-doctor-injecting-vaccine-patient-clinic_23-2148865135.jpg"></img>
-                    <Grid direction="column" style={{paddingTop: 110, paddingLeft: 80 }}>
+                    <img style={{width:672, height:672, marginLeft:40}} src="https://image.freepik.com/free-vector/illustration-doctor-injecting-vaccine-patient-clinic_23-2148865135.jpg"></img>
+                    <Grid direction="column" style={{paddingTop: 120, paddingLeft: 90 }}>
                     <div>
                     <h2 className="welcome_text">Prevent Better</h2>
                     <h2 className="welcome_text">Than Care </h2>
                     </div>
                     <button className="signin_btn">
                             <img style={{ width:20, height:20}} src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"></img>
-                            <Typography variant="caption" className="signin_text">Sign in with google</Typography>
+                            <Typography variant="button" style={{textTransform:"capitalize"}} className="signin_text">Sign in with google</Typography>
                     </button>
                     </Grid>
                 </Grid>
