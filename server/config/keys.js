049b0bfe0a6f3124140bc8682764aa83f0d5910f
coolkidneversleep <iiippbhy@gmail.com>
2021-06-26 14:20:45 +0700

    fix .gitignor

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    googleClientID: '791092496839-dbqcpg12asbue25eldj1b9ngtlrkfnvk.apps.googleusercontent.com',
-    googleClientSecret: 'rQv2wpNPef6qWXvA6QJ0A6uN'
-}
-
-//
-//https://accounts.google.com/o/oauth2/v2/auth?
-//redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
