387223c967ad6f1930dee9bc83098eb7e3758c1c
coolkidneversleep <iiippbhy@gmail.com>
2021-07-02 13:00:46 +0700

    moving to client side

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Component } from 'react/cjs/react.production.min';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import '../style/landing.scss'
@@ -20,10 +19,12 @@ const Landing = () => {
                     <h2 className="welcome_text">Prevent Better</h2>
                     <h2 className="welcome_text">Than Care </h2>
                     </div>
+                    <a href="/auth/google">
                     <button className="signin_btn">
                             <img style={{ width:20, height:20}} src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"></img>
                             <Typography variant="button" style={{textTransform:"capitalize"}} className="signin_text">Sign in with google</Typography>
                     </button>
+                    </a>
                     </Grid>
                 </Grid>
             </div>
