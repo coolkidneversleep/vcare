6cad232bb01cf327e2f4aedf6331dbe973130680
coolkidneversleep <iiippbhy@gmail.com>
2021-07-17 07:10:12 +0700

    edit: date format

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 import '../style/reservation.scss'
 
 const Confirmation = ({reservationInfo, date, location, vaccine}) => {
+    const day = date.getDate()
+    const month = date.getMonth()
+    const year = date.getFullYear()
     return(
         <div >
             <div className="small_card"style={{display: 'flex', flexFlow: 'column',justifyContent: 'center'}}>
                 <h3 className="confirm_text">Vaccine Reservation</h3>
             <div>{reservationInfo}</div>
-            <div>{date.toString()}</div>
+            <div>{day+"/"+month+"/"+year}</div>
             <div>{location}</div>
             <div>{vaccine}</div>
             
